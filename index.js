e64237e3da70a76ca5010a1571f84266c86b3eaa
joaquin <jecorrea@protonmail.com>
2021-04-29 08:04:46 -0400

    commit tree f7805b62b7bc03d4dcdb9140aa1e391788b7b66d

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+
+//function implemented with a function definition
+function saturdayFun(activity = `roller-skate`){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+//function implemented with a function expression
+const mondayWork = function(activity = `go to the office`) { return `This Monday, I will ${activity}.`;}
+
+//function definition 
+function wrapAdjective(flair = `*`){
+    return function(adj = `special`) {return `You are ${flair}${adj}${flair}!`};
+}
